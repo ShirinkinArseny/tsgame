8d05d2f065865473ad86b35f98f811f685d9b3a4
nameless <a-arsenij@yandex.ru>
2022-01-13 19:25:39 +0300

    Animation improved

--- assets/shaders/textured/fragment.glsl
+++ assets/shaders/textured/fragment.glsl
@@ -3,10 +3,7 @@ precision mediump float;
 varying vec2 v_texcoord;
 
 uniform sampler2D texture;
-uniform sampler2D mask;
 
 void main() {
-    vec4 c = texture2D(texture, v_texcoord);
-    c.a *= texture2D(mask, v_texcoord).r;
-    gl_FragColor = c;
+    gl_FragColor = texture2D(texture, v_texcoord);
 }
